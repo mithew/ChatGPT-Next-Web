0d4fc6cc611dbc40861df1987f6a188e2105fe83
mithew <43675269+mithew@users.noreply.github.com>
2023-05-06 13:21:58 +0800

    Update fetch-prompts.mjs

--- scripts/fetch-prompts.mjs
+++ scripts/fetch-prompts.mjs
@@ -4,9 +4,9 @@ import fs from "fs/promises";
 const RAW_FILE_URL = "https://raw.githubusercontent.com/";
 const MIRRORF_FILE_URL = "https://raw.fgit.ml/";
 
-const RAW_CN_URL = "mithew/1/main/prompts-zh.json";
+const RAW_CN_URL = "prompts-zh.json";
 const CN_URL = MIRRORF_FILE_URL + RAW_CN_URL;
-const RAW_EN_URL = "f-----------------/awesome-chatgpt-prompts/main/prompts.csv";
+const RAW_EN_URL = "prompts.csv";
 const EN_URL = MIRRORF_FILE_URL + RAW_EN_URL;
 const FILE = "./public/prompts.json";
 
