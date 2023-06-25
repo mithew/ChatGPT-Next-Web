9acb5baf06d5d4ed0f073b2b963607f25d0ad7e5
mithew <43675269+mithew@users.noreply.github.com>
2023-06-25 17:13:30 +0800

    Update config.ts

--- app/store/config.ts
+++ app/store/config.ts
@@ -57,23 +57,23 @@ export const ALL_MODELS = [
     available: ENABLE_GPT4,
   },
   {
-    name: "gpt-4-0314",
-    available: ENABLE_GPT4,
+    name: "gpt-4",
+    available: false,
   },
   {
     name: "gpt-4-32k",
     available: ENABLE_GPT4,
   },
   {
-    name: "gpt-4-32k-0314",
-    available: ENABLE_GPT4,
+    name: "gpt-4-32k",
+    available: false,
   },
   {
     name: "gpt-3.5-turbo",
     available: true,
   },
   {
-    name: "gpt-3.5-turbo-0301",
+    name: "gpt-3.5-turbo-16k",
     available: true,
   },
 ] as const;
